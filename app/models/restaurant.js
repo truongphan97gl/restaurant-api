7354dfa804df1f27eb4f700332013a4e7571d8e2
Truong <truongphan97gl@gmail.com>
2019-08-26 11:46:52 -0400

    - Created restaurant model

---
+++ app/models/restaurant.js
@@ -0,0 +1,25 @@
+const mongoose = require('mongoose')
+
+const restaurantSchema = new mongoose.Schema({
+  title: {
+    type: String,
+    required: true
+  },
+  text: {
+    type: String,
+    required: true
+  },
+  owner: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'User',
+    required: true
+  },
+  comment: [{
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'Comment'
+  }]
+}, {
+  timestamps: true
+})
+
+module.exports = mongoose.model('Restaurant', restaurantSchema)
